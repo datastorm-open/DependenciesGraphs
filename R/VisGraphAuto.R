a9534bf50ee48fd8c092b39a9e242418a0a3357e
bthieurmel <bthieurmel@gmail.com>
2015-04-29 14:52:36 +0200

    First datak update

--- R/VisGraphAuto.R
+++ R/VisGraphAuto.R
@@ -1,153 +1,180 @@
-require(visNetwork)
-require(mvbutils)
-
-
-#' Given a environment, can be a package, returns a vector of fonctions includes in this environment
-#' @param Envi : Environment, try fonction "search()" to obtain a list of charged environments
-#' @return nomfun : Names of fonctions includes in this environment.
+#' Given a environment, can be a package, returns a vector of included functions in this environment
+#' @param envir : Environment, try "search()" to obtain a list of loaded environments
+#' @return Names of included functions in this environment.
 #'
-#'
-AllFunctionEnv<-function(Envi){
-nomfun<-as.vector(ls.str(Envi,mode="function"))
-return(nomfun)
+#' @export
+allFunctionEnv <- function(envir){
+  functions.name <- as.vector(ls.str(envir, mode = "function"))
+  return(functions.name)
 }
 
-#' Given a matrix, returns a dataframe which two colums, first : master, second : slave
+#' Given a matrix, returns a dataframe which two columns 'master'and 'slave'
 #' @param Mat : matrix of dependencies, should be named (cols and rows), should be square matrix, dependencies are noted 1.
-#' @return M_s : Dataframe which two colums, first : master, second : slave
-#' @usage
-#' \code{require("mvbutils")}
-#' \code{graphfun<-foodweb( where="package:mvbutils",prune="foodweb",descendents=T,plotting =F,ancestors=F)$funmat}
-#' \code{MastersSlaves(graphfun)}
+#' @return Dataframe with two columns 'master'and 'slave'
 #'
-MastersSlaves <- function(Mat)
+#' @export
+mastersSlaves <- function(Mat)
 {
-  M_s <- apply(Mat,1, function(ligne){
+  M_s <- apply(Mat, 1, function(ligne){
     names(which(ligne == 1))
   })
-  M_s<-data.frame(master=rep(names(M_s),as.vector(unlist(lapply(M_s,length)))),slave=unlist(M_s,use.names = F))
+  M_s <- data.frame(master = rep(names(M_s), as.vector(unlist(lapply(M_s,length)))),
+                    slave = unlist(M_s, use.names = F))
   return(M_s)
 }
 
-#' For one fonction, give all dependencies
-#' @param Enviro : environment where the function should search dependencies.
-#' @param Nomfonciton : function name (character)
-#' @return ancdsc : Dataframe which two colums, first : master, second : slave
-#' @usage
-#' \code{require("mvbutils")}
-#' \code{LinksForOne(Enviro="package:mvbutils",Nomfonciton="foodweb")}
-#'
-LinksForOne <- function (Enviro,Nomfonciton){
-
-  graphfun<-mvbutils::foodweb( where=Enviro,prune=Nomfonciton,descendents=F,plotting =F,ancestors=T)$funmat
-
-  while (length(which(rowSums(as.matrix(graphfun))==0))!=1)
-     {
-       ligneasupr<-which(rowSums(graphfun)==0)[-which(Nomfonciton==names(which(rowSums(graphfun)==0)))]
-       graphfun<-graphfun[-ligneasupr,-ligneasupr]
-     }
-
-  ancestors<-MastersSlaves(as.matrix(graphfun))
-  graphfun<-mvbutils::foodweb( where=Enviro,prune=Nomfonciton,descendents=T,plotting =F,ancestors=F)$funmat
-
-      while (length(which(colSums(as.matrix(graphfun))==0))!=1)
-      {
-      ligneasupr<-which(colSums(graphfun)==0)[-which(Nomfonciton==names(which(colSums(graphfun)==0)))]
-      graphfun<-graphfun[-ligneasupr,-ligneasupr]
-    }
-
-
-  descendents<-MastersSlaves(as.matrix(graphfun))
-
-  if(length(ancestors)==1 || length(ancestors)==0)
-  { ancestors=NULL}
-  if(length(descendents)==1 || length(descendents)==0)
-  { descendents=NULL}
-
-  ancdsc<-data.frame(rbind(ancestors,descendents))
-
+#' For a function, give all dependencies
+#' @param envir : environment where the function should search dependencies.
+#' @param name.function : function name (character)
+#' @return Dataframe with two columns, 'master'and 'slave'
+#' 
+#' @importFrom mvbutils foodweb
+#' 
+#' @export
+linksForOne <- function (envir, name.function){
+  
+  current.warn.option <- options("warn")$warn
+  options(warn = -1)
+  
+  graphfun <- mvbutils::foodweb(where = envir, prune = name.function, descendents = F, plotting = F, ancestors = T)$funmat
+  
+  while(length(which(rowSums(as.matrix(graphfun)) == 0)) != 1)
+  {
+    ligneasupr <- which(rowSums(graphfun) == 0)[-which(name.function == names(which(rowSums(graphfun) == 0)))]
+    graphfun <- graphfun[-ligneasupr, -ligneasupr]
+  }
+  
+  ancestors <- mastersSlaves(as.matrix(graphfun))
+  graphfun <- mvbutils::foodweb(where = envir, prune = name.function, descendents = T, plotting = F, ancestors = F)$funmat
+  
+  while (length(which(colSums(as.matrix(graphfun)) == 0)) != 1)
+  {
+    ligneasupr <- which(colSums(graphfun) == 0)[-which(name.function==names(which(colSums(graphfun) == 0)))]
+    graphfun <- graphfun[-ligneasupr,-ligneasupr]
+  }
+  
+  descendents<-mastersSlaves(as.matrix(graphfun))
+  
+  if(length(ancestors) == 1 || length(ancestors) == 0){ancestors=NULL}
+  if(length(descendents) == 1 || length(descendents) == 0){descendents=NULL}
+  
+  ancdsc <- data.frame(rbind(ancestors,descendents))
+  
+  options(warn = current.warn.option)
   if(length(ancdsc)!=0)
   {
     colnames(ancdsc)=c("Master","Slaves")
-  return(ancdsc)
+    return(ancdsc)
   }
-else{
-  return(NULL)
+  else{
+    return(NULL)
+  }
 }
-}
-
-
-
 
 #' Prepare data for graph visNetwork
-#' @param Liens : Dataframe, two colums, the first is master, the second is slaves
-#' @param Nomfonctions : All fonctions to includes in graph (default : union(masters & slaves))
-#' @return visdata : List contain elements tu do a visNetwork graph
+#' @param  link : Dataframe, two colums, the first is master, the second is slaves
+#' @param functions : All functions to includes in graph (default : union(masters & slaves))
+#' @return List contain elements needed for visNetwork visualization
 #'
-PrepareToVis <- function(liens,Nomfonctions=NULL){
-  Visdata<-list()
-  if(is.null(Nomfonctions))
+#' @export
+prepareToVis <- function(link, functions = NULL){
+  Visdata <- list()
+  if(is.null(functions))
   {
-  Nomfun<-unique(as.vector(unlist(c(liens))))
-  Nomfun<-data.frame(cbind(id=1:length(Nomfun),label=Nomfun))
+    Nomfun <- unique(as.vector(unlist(c( link))))
+    Nomfun <- data.frame(cbind(id=1:length(Nomfun),label=Nomfun))
   }else{
-    Nomfun<-Nomfonctions
-    Nomfun<-data.frame(cbind(id=1:length(Nomfun),label=Nomfun))
+    Nomfun <- functions
+    Nomfun <- data.frame(cbind(id=1:length(Nomfun),label=Nomfun))
   }
-  fromto<-matrix(0,ncol=2,nrow=dim(liens)[1])
-
-  for(i in 1:dim(liens)[1])
+  fromto <- matrix(0,ncol=2,nrow=dim( link)[1])
+  
+  for(i in 1:dim( link)[1])
   {
-    fromto[i,1]<-which(as.character(liens[i,2])==Nomfun[,2])
-    fromto[i,2]<-which(as.character(liens[i,1])==Nomfun[,2])
+    fromto[i,1] <- which(as.character( link[i,2])==Nomfun[,2])
+    fromto[i,2] <- which(as.character( link[i,1])==Nomfun[,2])
   }
-  fromto<-data.frame(fromto)
-  names(fromto)<-c("from","to")
-  Visdata$Nomfun<-Nomfun
-  Visdata$fromto<-fromto
+  fromto <- data.frame(fromto)
+  names(fromto) <- c("from","to")
+  Visdata$Nomfun <- Nomfun
+  Visdata$fromto <- fromto
   return(Visdata)
 }
 
 
-#' Return all dependencies between a function and others functions in an envorinnement
-#' @param Enviro : envorinnement where you want to scherch dependencies
-#' @param Func : Function name (in character)
-#' @return visdata : List contain elements tu do a visNetwork graph
+#' Return all dependencies from a function in an environment
+#' @param envir : environment where you want to scherch dependencies
+#' @param name.function : Function name (in character)
+#' @return List with nodes and edges informations. Needed for visNetwork visualization.
 #'
-VisFun<- function(Enviro,Func)
+#' @examples
+#' 
+#' dep <- funDependencies("package:ibr","iterchoiceS1")
+#' plot(dep)
+#' 
+#' @export
+funDependencies <- function(envir, name.function)
 {
-  visdata<-PrepareToVis(LinksForOne(Enviro,Func))
+  visdata <- prepareToVis(linksForOne(envir, name.function))
+  class(visdata) <- "dependenciesGraph"
   return(visdata)
 }
 
 
-#' Return all dependencies between fonctions in an envorinnement
-#' @param Enviro : envorinnement where you want to scherch dependencies
-#' @return visdata : List contain elements to do a visNetwork graph
-#'
-VisFuns<- function(Enviro)
+#' Return all dependencies between functions in an environment
+#' @param envir : environment on which you want to search dependencies
+#' @return List with nodes and edges informations. Needed for visNetwork visualization.
+#' @examples
+#' 
+#' dep <- envirDependencies("package:ibr")
+#' plot(dep)
+#' 
+#' @export
+envirDependencies <- function(envir)
 {
-  Nomfonctions<-AllFunctionEnv(Enviro)
-  toutfonc<-NULL
-  for(ww in 1:length(Nomfonctions))
-  {
-    print(ww)
-    toutfonc<-rbind(toutfonc,LinksForOne(Enviro,Nomfonctions[ww]))
-  }
-  visdata<-PrepareToVis(unique(toutfonc),Nomfonctions)
+  name.functions <- allFunctionEnv(envir)
+
+  toutfonc <- do.call("rbind", lapply( name.functions, function(x){
+    linksForOne(envir,x)})
+  )
+  
+  visdata <- prepareToVis(unique(toutfonc), name.functions)
+  class(visdata) <- "dependenciesGraph"
   return(visdata)
 }
 
 
 #' Return all dependencies between elements of a matrix
 #' @param Mat : matrix of dependencies, should be named (cols and rows), should be square matrix, dependencies are noted 1.
-#' @return visdata : List contain elements to do a visNetwork graph
+#' @return List with nodes and edges informations. Needed for visNetwork visualization.
 #'
-VisFunsmatrice<- function(Mat)
+#' @export
+VisFunsmatrice <- function(Mat)
 {
-  Nomfonctions<-AllFunctionEnv(Enviro)
-  toutfonc<-MastersSlaves(Mat)
-
-  visdata<-PrepareToVis(unique(toutfonc),colnames(Mat))
+  
+  data.mat <- mastersSlaves(Mat)
+  
+  visdata <- prepareToVis(unique(data.mat), colnames(Mat))
+  class(visdata) <- "dependenciesGraph"
   return(visdata)
+}
+
+#' Plot network for dependenciesGraph object
+#' 
+#' Plot network for dependenciesGraph object. Using visNetwork package.
+#' 
+#' Plot network for dependenciesGraph object. Using visNetwork package.
+#' 
+#' @param object : dependenciesGraph object. 
+#' 
+#' @examples
+#' 
+#' dep <- funDependencies("package:ibr","iterchoiceS1")
+#' plot(dep)
+#' 
+#' @export 
+#' @method plot dependenciesGraph
+plot.dependenciesGraph <- function(object){
+  visNetwork(object[[1]], object[[2]]) %>% visEdges(style = "arrow") %>%
+    visOptions(highlightNearest = TRUE, nodesIdSelection = TRUE)
 }
