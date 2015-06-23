5b324a3d62d92e69d752d08b9317096be128ed0e
bthieurmel <bthieurmel@gmail.com>
2015-06-23 10:23:55 +0200

    update shiny app

--- inst/Shiny/SERVER.R
+++ inst/Shiny/SERVER.R
@@ -6,15 +6,15 @@ shinyServer(function(input, output,session) {
   
   
   observe({
-    input$GOb
+    input$GOPackage
     isolate({
-      print(input$Pack)
-      if(length(input$Pack)>0)
+      # print(input$Pack)
+      if(length(input$packages)>0)
       {
-        data<-Pck.load.to.vis(input$Pack)
+        data<-Pck.load.to.vis(input$packages)
         
-        func<-c(input$Pack)
-        print(func)
+        func<-c(input$packages)
+        # print(func)
         
         
         nb.func.slave=NULL
@@ -44,27 +44,30 @@ shinyServer(function(input, output,session) {
         
         output$tabledep <- renderDataTable({df},
                                            options=optionsDT_fixe)
-        print(data)
+        # print(data)
         output$main_plot <- renderVisNetwork({plot(data,block=TRUE)})
         curentd1<<-data
-        output$titledatatabel<-renderText({"Depends between package(s)"})
+        output$titledatatabel<-renderText({"Dependencies between package(s)"})
         
       }
     })
   })
   
   
-  
+  observe({
+    current.package <- input$main_plot_selected
+    current.package <- as.character(curentd1$Nomfun[as.numeric(current.package), "label"])
+    updateSelectizeInput(session, "package", NULL, choices = installed.packages()[,1], selected = current.package)
+  })
   
   observe({
-    input$zoom
+    input$GOFunc2
     isolate({
-      
-      if(!is.null(input$main_plot_selected) && input$main_plot_selected!="" && as.numeric(input$zoom>0))
+      if(input$package!="" && input$GOFunc2 > 0)
       {
         
-        func<-as.character(curentd1$Nomfun$label[input$main_plot_selected==curentd1$Nomfun$id])
-        print(func)
+        func<-input$package
+        # print(func)
         func
         
         if(!func%in%installed.packages()[,1])
@@ -88,9 +91,41 @@ shinyServer(function(input, output,session) {
         curentd2<<-dep1
       }
     })
-    
   })
-    
+  
+  observe({
+    input$GOFunc1
+    isolate({
+      if(!is.null(input$main_plot_selected) && input$main_plot_selected!="" && input$GOFunc1 > 0)
+      {
+        
+        func<-as.character(curentd1$Nomfun$label[input$main_plot_selected==curentd1$Nomfun$id])
+        # print(func)
+        func
+        
+        if(!func%in%installed.packages()[,1])
+        {
+          install.packages(func)
+        }
+        library(func,character.only = TRUE)
+        dep1 <- envirDependencies(paste0("package:",func))
+        nb.fun<-length(dep1$Nomfun$label)
+        
+        
+        updateTabsetPanel(session,"Tabsetpan", selected = "Functions")
+        optionsDT_fixe$drawCallback<-I("function( settings ) {document.getElementById('datatable2').style.width = '100px';}")
+        output$datatable2<-renderDataTable(data.frame(Number.of.functions=nb.fun),options=optionsDT_fixe)
+        
+        output$zoomin<-renderText(paste("Zoom on package : ",func))
+        output$info<-renderText(paste("Information on : ",func))
+        curentd3<<-func
+        
+        output$main_plot1<-renderVisNetwork({plot(dep1,block=TRUE)})
+        curentd2<<-dep1
+      }
+    })
+  })
+  
   observe({
     
     if(!is.null(input$main_plot1_selected) && input$main_plot1_selected!="")
@@ -98,10 +133,10 @@ shinyServer(function(input, output,session) {
       isolate({
         pck<-curentd3
         
-        print(pck)
+        # print(pck)
         
         func<-as.character(curentd2$Nomfun$label[input$main_plot1_selected==curentd2$Nomfun$id])
-        print(func)
+        # print(func)
         try(add.html.help(pck,func),TRUE)
         
         if(length(htmlTreeParse(paste0(getwd(),"/temp.html"))$children$html)>0)
@@ -114,60 +149,52 @@ shinyServer(function(input, output,session) {
         }
       })
     }else{
-
-        output$help<-renderUI("Select a function")
-      }
       
-    })
-
-observe({
-  
-  if(!is.null(input$main_plot_selected) && input$main_plot_selected!="")
-  {
+      output$help<-renderUI("Select a function")
+    }
     
-    func<-as.character(curentd1$Nomfun$label[input$main_plot_selected==curentd1$Nomfun$id])
-    
-    output$Groupebutton<-renderUI({      
-
-        div(hr(), actionButton("zoom", paste0("Launch zoom on : ", func),icon = icon("line-chart")), align = "center")
-
-    })
-  }
-  else
-  {
-    output$Groupebutton<-renderUI({NULL})
-  }
-  
-})
-
-
-
-observe({
-
-  input$GObott
-  # input$file1 will be NULL initially. After the user selects
-  # and uploads a file, it will be a data frame with 'name',
-  # 'size', 'type', and 'datapath' columns. The 'datapath'
-  # column will contain the local filenames where the data can
-  # be found.
-
-  inFile <- input$file1
-  
-  if(!is.null(inFile))
-  {
-    print("lala")
-  dep <- data.graph.script(inFile$datapath)
-  output$plotscript <- renderVisNetwork({plot(dep,block=TRUE)})
-  
-  }
-  
-
-  
-})
-
-
-
   })
+  
+  observe({
+    
+    if(!is.null(input$main_plot_selected) && input$main_plot_selected!="")
+    {
+      
+      func<-as.character(curentd1$Nomfun$label[input$main_plot_selected==curentd1$Nomfun$id])
+      
+      output$Groupebutton<-renderUI({      
+        
+        div(hr(), actionButton("GOFunc1", paste0("Launch zoom on : ", func),icon = icon("line-chart")), align = "center")
+        
+      })
+    }
+    else
+    {
+      output$Groupebutton<-renderUI({NULL})
+    }
+    
+  })
+  
+  
+  
+  observe({
+    
+    input$GObott
+    # input$file1 will be NULL initially. After the user selects
+    # and uploads a file, it will be a data frame with 'name',
+    # 'size', 'type', and 'datapath' columns. The 'datapath'
+    # column will contain the local filenames where the data can
+    # be found.
+    
+    inFile <- input$file1
+    
+    if(!is.null(inFile))
+    {
+      dep <- data.graph.script(inFile$datapath)
+      output$plotscript <- renderVisNetwork({plot(dep,block=TRUE)})
+    }
+  })
+})
 
 
 
--- inst/Shiny/UI.R
+++ inst/Shiny/UI.R
@@ -1,53 +1,48 @@
 dashboardPage(
-  dashboardHeader(title = "DataK depends Graphs"),
-  dashboardSidebar( disable = TRUE
-                    
-  ),
+  dashboardHeader(title = "DependenciesGraphs"),
+  dashboardSidebar( disable = TRUE),
   dashboardBody(
     # Boxes need to be put in a row (or column)
     tags$head(tags$link(rel='stylesheet', type='text/css', href='style.css')),
-    
     
     tabsetPanel(id = "Tabsetpan",  title = "Packages exploration", width = "100%",
                 # The id lets us use input$tabset1 on the server to find the current tab 
                 tabPanel("Packages",
                          fluidRow(
-                           
-                           fluidRow(
-
-                             column(4, div(h3('Package(s) :'), align = "center")),
-                             column(4, br(), selectizeInput('Pack', NULL, choices = installed.packages()[,1], multiple = T)),
-                             column(4, br(), div(actionButton("GOb", "Launch",icon = icon("line-chart")), align = "center"))
+                           column(4, div(h3('Package(s) :'), align = "center")),
+                           column(4, br(), selectizeInput('packages', NULL, choices = installed.packages()[,1], multiple = T)),
+                           column(4, br(), div(actionButton("GOPackage", "Launch",icon = icon("line-chart")), align = "center"))
+                         ),
+                         hr(),
+                         
+                         fluidRow(
+                           column(
+                             box(
+                               h3("Dependencies between package(s)"),
+                               visNetworkOutput("main_plot", width = "100%",height = "600px")
+                               ,width = 12
                              ),
+                             width=8),
                            
-                           fluidRow(
-                             column(
-                               box(
-                                 h3("Depends between package(s)"),
-                                 visNetworkOutput("main_plot", width = "100%",height = "600px")
-                                 ,width = 12),
-                               width=8),
-                             
-                             column(
-                               div(
-                                 uiOutput("Groupebutton")
-                               ),
-                               
-                               
-                               div(
-                                 
-                                 h3( textOutput("titledatatabel")),
-                                 dataTableOutput("tabledep")
-                                 ,align="center"
-                               )
-                               , 
-                               width=4)
-                           )
+                           column(
+                             div(
+                               h3(textOutput("titledatatabel")),
+                               dataTableOutput("tabledep"),
+                               uiOutput("Groupebutton"),
+                               align="center"
+                             ), 
+                             width=4)
                          )
-                         
-                         
                 ),
                 tabPanel("Functions",
+                         
+                         fluidRow(
+                           column(4, div(h3('Package :'), align = "center")),
+                           column(4, br(), selectizeInput('package', NULL, choices = installed.packages()[,1], multiple = FALSE)),
+                           column(4, br(), div(actionButton("GOFunc2", "Launch",icon = icon("line-chart")), align = "center"))
+                         ),
+                         hr(),
+                         
                          fluidRow(
                            box(
                              h3(textOutput({"zoomin"})),
@@ -69,15 +64,15 @@ dashboardPage(
                          )
                 ),
                 tabPanel("Script",
-     
-               
-                fluidRow(
-                  box(
-                    fileInput('file1', 'Choose R File',
-                              accept=NULL),
-                    visNetworkOutput("plotscript", width = "100%",height = "700px")
-                    ,width = 12)
-                )
+                         
+                         
+                         fluidRow(
+                           box(
+                             fileInput('file1', 'Choose R File',
+                                       accept=NULL),
+                             visNetworkOutput("plotscript", width = "100%",height = "700px")
+                             ,width = 12)
+                         )
                 )
     )
   )
